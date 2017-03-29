784b9d07944f13cf9428c9fd55cc848d46118581
mihailsoft <mg@rademade.com>
2017-03-29 18:43:40 +0300

    index hackatons done

--- frontend/src/app/containers/hackathons/index.jsx
+++ frontend/src/app/containers/hackathons/index.jsx
@@ -5,8 +5,12 @@ import {bindActionCreators} from 'redux'
 import Formsy from 'formsy-react'
 import {FormsyText, FormsyToggle} from 'formsy-material-ui/lib'
 import RaisedButton from 'material-ui/RaisedButton'
-import Paper from 'material-ui/Paper'
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn, FlatButton } from 'material-ui'
 import * as authActions from 'actions/auth'
+// import {moment} form 'moment'
+import moment from 'moment'
+
+moment.locale('ru');
 
 const styles = {
   paperStyle: {
@@ -23,6 +27,38 @@ const styles = {
   }
 }
 
+const hackstons = [
+  {
+    id: 1,
+    topic: 'afsadf',
+    date: new Date(),
+    speaker: 'asfasg',
+    done: true,
+    materials_link: 'http://ya.ru'
+  },
+  {
+    id: 2,
+    topic: 'afsadf',
+    date: new Date(),
+    speaker: 'asfasg',
+    done: true,
+    materials_link: 'http://ya.ru'
+  },
+  {
+    id: 3,
+    topic: 'afsadf',
+    date: new Date(),
+    speaker: 'asfasg',
+    done: false,
+    materials_link: 'http://ya.ru'
+  }
+];
+
+const isAdmin = ()=>{
+  return true;
+};
+
+
 const mapStateToProps = (state, ownProps) => ({
   state: state.auth
 })
@@ -32,9 +68,38 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
 })
 
 const HackathonIndex = ({state, actions}) => (
-  <Paper style={styles.paperStyle}>
-    HackathonIndex
-  </Paper>
+  <div>
+    <h1>Список хакатонов</h1>
+    <Table>
+      <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+        <TableRow>
+          <TableHeaderColumn>Тема</TableHeaderColumn>
+          <TableHeaderColumn>Дата</TableHeaderColumn>
+          <TableHeaderColumn>Докладчик</TableHeaderColumn>
+          <TableHeaderColumn>Статус</TableHeaderColumn>
+          {isAdmin() &&<TableHeaderColumn>Действия</TableHeaderColumn>}
+        </TableRow>
+      </TableHeader>  
+      <TableBody displayRowCheckbox={false}>
+      {hackstons.map(
+        (item)=>{
+          return (<TableRow key={item.id}>
+                  <TableRowColumn>{item.topic}</TableRowColumn>
+                  <TableRowColumn>{moment(item.date).format('DD, MMM')}</TableRowColumn>
+                  <TableRowColumn>{item.speaker}</TableRowColumn>
+                  <TableRowColumn>{item.done ?
+                    <span>Завершен, материалы: <a href={item.materials_link}>тут</a> </span>
+                    :
+                    'Еще не проведен'
+                    }</TableRowColumn>
+                  {isAdmin() && <TableRowColumn><FlatButton label="Изменить" primary={true} /></TableRowColumn>}
+                </TableRow>)
+        }
+      )
+      }
+    </TableBody>
+    </Table>
+  </div>
 )
 
 export default connect(
