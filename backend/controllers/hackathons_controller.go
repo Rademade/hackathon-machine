949431f276a2334508e86c0a2ed2dbb6865bfde6
Praffesor <kuzh@rademade.com>
2017-04-05 18:49:31 +0300

    added destroy actions for rest classes, some fixes

---
+++ backend/controllers/hackathons_controller.go
@@ -0,0 +1,82 @@
+package controllers
+
+import (
+	"github.com/hackathon-machine/backend/models"
+	"github.com/labstack/echo"
+)
+
+type HackathonsController struct {
+}
+
+func (u HackathonsController) Index() interface{} {
+
+	var hackathons []models.Hackathon
+
+	models.DB.Preload("Topic.Creator").Preload("Speaker").Find(&hackathons)
+
+	return hackathons
+
+}
+
+func (u HackathonsController) Create(c echo.Context) (interface{}, error) {
+
+	hackathon := models.Hackathon{}
+
+	if err := c.Bind(&hackathon); err != nil {
+		return nil, err
+	}
+
+	if err := models.DB.Create(&hackathon).Error; err != nil {
+		return nil, err
+	}
+
+	return hackathon, nil
+
+}
+
+func (u HackathonsController) Show(c echo.Context) (interface{}, error) {
+
+	hackathon := models.Hackathon{}
+
+	if err := models.DB.First(&hackathon, c.Param("id")).Error; err != nil {
+		return hackathon, err
+	}
+
+	return hackathon, nil
+
+}
+
+func (u HackathonsController) Update(c echo.Context) (interface{}, error) {
+
+	hackathon := models.Hackathon{}
+
+	if err := models.DB.Preload("Topic").First(&hackathon, c.Param("id")).Error; err != nil {
+		return hackathon, err
+	}
+
+	if err := c.Bind(&hackathon); err != nil {
+		return hackathon, err
+	}
+
+	if err := models.DB.Save(&hackathon).Error; err != nil {
+		return hackathon, err
+	}
+
+	return hackathon, nil
+}
+
+func (u HackathonsController) Destroy(c echo.Context) error {
+
+	hackathon := models.Hackathon{}
+
+	if err := models.DB.First(&hackathon, c.Param("id")).Error; err != nil {
+		return err
+	}
+
+	if err := models.DB.Delete(&hackathon).Error; err != nil {
+		return err
+	}
+
+	return nil
+
+}
--- backend/controllers/topics_controller.go
+++ backend/controllers/topics_controller.go
@@ -68,6 +68,16 @@ func (u TopicsController) Update(c echo.Context) (interface{}, error) {
 
 func (u TopicsController) Destroy(c echo.Context) error {
 
+	topic := models.Topic{}
+
+	if err := models.DB.First(&topic, c.Param("id")).Error; err != nil {
+		return err
+	}
+
+	if err := models.DB.Delete(&topic).Error; err != nil {
+		return err
+	}
+
 	return nil
 
 }
