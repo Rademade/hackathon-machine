50a673a3335f7743f156c4f8939866bc34a60c51
robobee <igor.serpak@gmail.com>
2017-04-27 18:40:38 +0300

    Small refactor

--- backend/controllers/user_votes_controller.go
+++ backend/controllers/user_votes_controller.go
@@ -10,23 +10,27 @@ import (
 type UserVotesController struct {
 }
 
-func (u UserVotesController) Create(c echo.Context) (interface{}, error) {
-
+func authorizeUser(c echo.Context, recordUserId int) bool {
 	tokenUser := c.Get("user").(*jwt.Token)
 	claims := tokenUser.Claims.(jwt.MapClaims)
 	userId := int(claims["id"].(float64))
+
+	return userId == recordUserId;
+}
+
+func (u UserVotesController) Create(c echo.Context) (interface{}, error) {
 
 	userVote := models.UserVote{}
 	if err := c.Bind(&userVote); err != nil {
 		return userVote, err
 	}
 
-	if userVote.Vote < 0 || userVote.Vote > 5 {
-		return nil, errors.New("invalid vote")
+	if !authorizeUser(c, userVote.UserId) {
+		return nil, echo.ErrUnauthorized
 	}
 
-	if userId != userVote.UserId {
-		return nil, echo.ErrUnauthorized
+	if userVote.Vote < 0 || userVote.Vote > 5 {
+		return nil, errors.New("invalid vote")
 	}
 
 	if err := models.DB.Create(&userVote).Error; err != nil {
@@ -39,21 +43,17 @@ func (u UserVotesController) Create(c echo.Context) (interface{}, error) {
 
 func (u UserVotesController) Update(c echo.Context) (interface{}, error) {
 
-	tokenUser := c.Get("user").(*jwt.Token)
-	claims := tokenUser.Claims.(jwt.MapClaims)
-	userId := int(claims["id"].(float64))
-
 	userVote := models.UserVote{}
 	if err := models.DB.First(&userVote, c.Param("id")).Error; err != nil {
 		return userVote, err
 	}
 
-	if userId != userVote.UserId {
-		return nil, echo.ErrUnauthorized
-	}
-
 	if err := c.Bind(&userVote); err != nil {
 		return userVote, err
+	}
+
+	if !authorizeUser(c, userVote.UserId) {
+		return nil, echo.ErrUnauthorized
 	}
 
 	if userVote.Vote < 0 || userVote.Vote > 5 {
