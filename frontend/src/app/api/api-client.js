0b85798dae2ea0ea33729d9895f944b0636696e9
kach95 <paliukh.mykhailo@domain.com>
2017-05-09 17:08:35 +0300

    moved axios to class getter

--- frontend/src/app/api/api-client.js
+++ frontend/src/app/api/api-client.js
@@ -13,12 +13,14 @@ export class ApiClient {
   constructor(resourceRelativePath = '') {
     const endpoint = AppSettings.API_ENDPOINT + resourceRelativePath
 
-    this.http = axios
-
     this.query = _ => this.http.get(endpoint, config)
     this.get = id => this.http.get(join(endpoint, id), config)
     this.create = data => this.http.post(endpoint, data, config)
     this.update = data => this.http.put(join(endpoint, data.id), data, config)
     this.delete = id => this.http.delete(join(endpoint, id), config)
+  }
+
+  get http() {
+    return axios;
   }
 }
